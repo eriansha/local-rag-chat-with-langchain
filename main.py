df43fa0122c0464dbcbf5dcad368f27fe075c4ac
Ivan Putra Eriansya <eriansha.van@gmail.com>
2025-06-04 15:17:42 +0700

    chore: remove unused import

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 from dotenv import load_dotenv
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
